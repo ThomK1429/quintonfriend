3ed3e0c18d096e7e2203953785f1e113a57bd31e
ThomK1429 <sndbytgkv1@verizon.net>
2019-09-09 21:51:48 -0400

    init file load

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Robin",
+		photo: "https://i.pinimg.com/originals/27/e4/85/27e485db779af2d7b622995b2f669a15.jpg",
+		scores: [5, 4, 3, 5, 2, 2, 2, 4, 2, 2]
+	},
+	{
+		name: "Cyborg",
+		photo: "https://cdn.vox-cdn.com/thumbor/515aklStg73mrHHqDnRaMUqKROw=/0x0:1920x1080/1200x800/filters:focal(802x210:1108x516)/cdn.vox-cdn.com/uploads/chorus_image/image/57555003/cyborg.0.png",
+		scores: [4, 5, 2, 3, 4, 5, 4, 2, 4, 4]
+	},
+	{
+		name: "Starfire",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaB7z5GUHM16znZ2hWMvx2RUYZSxNJYU96xOZNrwspp6zNEpo3",
+		scores: [2, 3, 1, 1, 1, 1, 1, 5, 4, 1]
+	},
+	{
+		name: "Raven",
+		photo: "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2019/02/raven_teen_titans.png",
+		scores: [3, 2, 5, 2, 3, 3, 1, 1, 5, 5]
+	},
+	{
+		name: "Beast Boy",
+		photo: "http://colorwebmag.com/wp-content/uploads/2018/04/beast-boy.jpg",
+		scores: [1, 1, 4, 4, 5, 4, 5, 1, 3, 3]
+	}
+];
+
+module.exports = friends;
